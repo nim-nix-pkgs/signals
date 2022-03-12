6ab077dc95085f0ce9b1d3d2e6076341422f9367
hugosenari <hugosenari@gmail.com>
2022-03-12 04:07:31 -0300

    chore: re index signals

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Signals/slots library.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."signals-master".url = "path:./master";
+  inputs."signals-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."signals-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Signals/slots library.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-signals-master.flake = false;
+  inputs.src-signals-master.owner = "fowlmouth";
+  inputs.src-signals-master.ref   = "refs/heads/master";
+  inputs.src-signals-master.repo  = "signals.nim";
+  inputs.src-signals-master.type  = "github";
+  
+  inputs."nimrod".dir   = "nimpkgs/n/nimrod";
+  inputs."nimrod".owner = "riinr";
+  inputs."nimrod".ref   = "flake-pinning";
+  inputs."nimrod".repo  = "flake-nimble";
+  inputs."nimrod".type  = "github";
+  inputs."nimrod".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimrod".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-signals-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-signals-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
